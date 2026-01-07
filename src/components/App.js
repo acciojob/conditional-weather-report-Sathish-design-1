5fe3b1dde26d1d68a7d13b4bee92de74a39f0847
SATHISH B <sathishb2310@gmail.com>
2026-01-07 23:35:16 +0530

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -1,15 +1,17 @@
-import React, { useState } from 'react';
-import WeatherDisplay from './WeatherDisplay';
+import { useState } from "react";
+import WeatherDisplay from "./WeatherDisplay";
 
 function App() {
-    const [weather] = useState({ temperature: 25, conditions: "Sunny" });
+  const [weather] = useState({
+    temperature: 25,
+    conditions: "Sunny"
+  });
 
-    return (
-        <div>
-            <h1>Weather Display</h1>
-            <WeatherDisplay weather={weather} />
-        </div>
-    );
+  return (
+    <div>
+      <WeatherDisplay weather={weather} />
+    </div>
+  );
 }
 
 export default App;
